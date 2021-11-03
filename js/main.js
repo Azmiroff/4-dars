4d26896303d5ed3c9691eef4accde8faeabb8383
Azmiroff <Mirsoliev9099@gmail.com>
2021-11-03 13:38:38 +0500

    "add js cod"

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var userName = prompt("Imingiz?");
+console.log(userName , typeof userName);
+alert("Xush " + "kelibsiz " +  userName + "!" + " Davom etamizmi?");
+
+var userSurname = prompt("Familyangiz?");
+console.log(userSurname , typeof userSurname);
+alert("Tanishganimdan hursandman" + " " + userSurname +" " + userName);
+
+var userAge = prompt("Yoshingiz?");
+console.log(userAge , typeof userAge);
+
+var userRegion = prompt("Viloyatingiz?");
+console.log(userRegion , typeof userRegion);
+
+var userStreet = prompt("Mahallangiz?");
+console.log(userStreet , typeof userStreet);
+
+var userCar = prompt("Qanaqa moshina sotib olishni hohlaysiz?");
+console.log(userCar , typeof userCar);
+
+var userCarColor = prompt("Qanaqa rang bo`lishini hohlaysiz?");
+console.log(userCarColor , typeof userCarColor);
+
+var userCarDate = prompt("Moshinani qaysi sanaga olishni hohlaysiz?");
+console.log(userCarDate , typeof userCarDate);
+
+var userCarInfo = prompt("Firma xaqida malumotni qayerdan eshitdingiz?");
+console.log(userCarInfo , typeof userCarInfo);
+
+var userCompanyInfo = prompt("Firma xaqida fikringiz?");
+console.log(userCompanyInfo , typeof userCompanyInfo);
+alert("Tanishganimdan hursandman" + " " + userSurname + " " + userName + " Sizga " + userCarDate + " kuni " + userCarColor + " rangli " + userCar + "ingizni " + userRegion + " viloyati " + userStreet + " mahallasiga oborib beramiz!")
